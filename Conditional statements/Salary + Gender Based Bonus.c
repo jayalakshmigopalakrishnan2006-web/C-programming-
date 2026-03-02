70152c9dbdcde56a3832eaa29308ed794debe1a8
G.P.Jayalakshmi <jayalakshmigopalakrishnan2006@gmail.com>
2026-03-02 08:18:41 +0530

    Create Salary + Gender Based Bonus.c

---
+++ Conditional statements/Salary + Gender Based Bonus.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    float salary, bonus, finalSalary;
+    char gender;
+
+    printf("Enter salary: ");
+    scanf("%f", &salary);
+
+    printf("Enter gender (M/F): ");
+    scanf(" %c", &gender);
+
+    if (gender == 'M' || gender == 'm')
+        bonus = salary * 0.10;
+    else if (gender == 'F' || gender == 'f')
+        bonus = salary * 0.15;
+    else {
+        printf("Invalid gender input!\n");
+        return 0;
+    }
+
+    finalSalary = salary + bonus;
+
+    printf("Final Salary = %.2f\n", finalSalary);
+
+    return 0;
+}
